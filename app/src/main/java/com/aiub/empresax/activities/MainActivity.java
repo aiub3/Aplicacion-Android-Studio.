1d51a1676b6904ce1d7c43d6f8304447620116c1
Aiub Al-lal <aiub.1997ceuta@gmail.com>
2018-09-08 00:33:40 +0200

    Código final.

--- app/src/main/java/com/aiub/empresax/activities/MainActivity.java
+++ app/src/main/java/com/aiub/empresax/activities/MainActivity.java
@@ -9,11 +9,6 @@ import android.view.Menu;
 import com.aiub.empresax.R;
 import com.aiub.empresax.adapter.Adapter;
 
-// TOOLBAR ES LA FORMA CORRECTA DE CREAR BARRAS DE HERRAMIENTAS ARRIBA, TIENE MUCHAS FORMAS DE PERSONALIZARSE Y
-// TIENE MAS COMPATIBILIDAD QUE LA ACTIONBAR QUE ES LA QUE SUELE HABER POR DEFECTO EN ANDROID
-// PARA IMPLEMENTAR EL TOOLBAR LO PRIMERO QUE HAY QUE HACER ES QUITAR EL ACTIONBAR EN STYLES.XML
-// Y AÑADIR EL CODIGO QUE INDICA LA DOCUMENTACION OFICIAL EN EL LAYOUT DEL ACTIVITY EN QUE QUERAMOS AÑADIR EL TOOLBAR
-// HAY MAS INFORMACION EN LA DOCUMENTACION OFICIAL DE GOOGLE
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,34 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ESTE CODIGO TAMBIEN ES COPIADO DE LA DOCUMENTACION OFICIAL, IMPORTAMOS LA LIBRERIA TOOLBAR DE ANDROID.SUPPORT.V7.WIDGET
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        // A la variabl etabLayout que creamos le añadimos un tabs nuevos y a cada uno un texto
         tabLayout.addTab(tabLayout.newTab().setText(R.string.first_fragment));
         tabLayout.addTab(tabLayout.newTab().setText(R.string.second_fragment));
         tabLayout.addTab(tabLayout.newTab().setText(R.string.third_fragment));
-        // Le añadimos este elemento para que mejore el diseño del tabLayout
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        // AUTOMATICAMENTE ESTA VARIABLE SE CONVIERTE EN FINAL PORQUE ES NECESARIO PARA PODER
-        // USARLA DENTRO DEL METODO ONTABSELECTED
         final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        // CREAMOS UNA INSTANCIA DEL ADAPTADOR QUE HEMOS CREADO PASANDOLE LOS 2 ARGUMENTOS, EL PRIMERO ES EL QUE SIEMPRE VA A NECESITAR
-        // Y EL SEGUNDO ES QUE AÑADIMOS, EN ESTE CASO EL NUMERO DE TABS
         Adapter adapter = new Adapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        // LE AÑADIMOS EL ADAPTADOR AL VIEWPAGER
         viewPager.setAdapter(adapter);
-        // CADA VEZ QUE CAMBIEMOS DE TAB EL VIEWPAGER TAMBIEN CAMBIA DE FRAGMENT
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        // CUANDO EL HAYA ALGUNA ACCION SOBRE EL TAB PODEMOS AÑADIR FUNCIONES CON ESTE METODO
-        // LA PRIMERA SE ACTIVA AL SELECCIONAR UN TAB DIFERENTE AL QUE ESTAMOS, EN EL TAB AL QUE ACCEDEMOS
-        // LA SEGUNDA CUANDO SELECCIONAMOS UN TAB DIFERENTE, PERO OCURE EN EL TAB EN QUE VAMOS A DEJAR DE ESTAR
-        // EL TERCERO CUANDO PULSAMOS EN EL MISMO TAB DONDE ESTAMOS
-        // LOS 3 METODOS SON NECESARIOS Y SE SOBREESCRIBEN AUTOMATICAMENTE
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -68,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    // EL MENU SON LOS 3 PUNTOS A LA DERRECHA ARRIBA QUE AL PULSAR APARECE UN MENU
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
--- app/src/main/java/com/aiub/empresax/adapter/Adapter.java
+++ app/src/main/java/com/aiub/empresax/adapter/Adapter.java
@@ -10,20 +10,13 @@ import com.aiub.empresax.fragments.ThirdFragment;
 
 public class Adapter extends FragmentStatePagerAdapter {
 
-    // ESTE ADAPTADOR ES NECESARIO PARA EL VIEWPAGER
-
     private int numberOfTabs;
 
-    // ESTE CONSTRUCTOR HAY QUE IMPLEMENTARLO DE LA CLASE PADRE, ES NECESARIO
-    // EL PARAMETRO tabs LO IMPLEMENTAMOS SOLO PARA ESTE CASO, NO SE IMPLEMENTA AUTOMATICAMENTE
     public Adapter(FragmentManager fm, int tabs) {
         super(fm);
         this.numberOfTabs = tabs;
     }
 
-    // ESTOS METODOS HAY QUE IMPLEMENTARLOS DE LA CLASE PADRE, ES NECESARIO
-
-    // CON ESTE METODO DEPENDIENDO DEL TAB DONDE ESTEMOS IREMOS A UN FRAGMENT
     @Override
     public Fragment getItem(int position) {
 
@@ -39,7 +32,6 @@ public class Adapter extends FragmentStatePagerAdapter {
         }
     }
 
-    // POR DEFECTO ESTE METODO DEVOLVIA 0 PERO SE LO CMABIAMOS POR LA VARIABLE NUMBREOFTABS
     @Override
     public int getCount() {
         return numberOfTabs;
